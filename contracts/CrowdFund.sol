2c9fe03d14b60a48706ad1c28de5e8b2da2bf054
KhalilAsghar544 <khalil.asghar54@gmail.com>
2023-01-18 18:47:04 +0500

    comments added

--- contracts/CrowdFund.sol
+++ contracts/CrowdFund.sol
@@ -18,7 +18,7 @@ interface IERC20 {
     */
 
 contract CrowdFund {
-    struct Campaign {
+    struct Campaign {       //- A struct for storing data on the campaign's creator, aim, start time, end time, and the pledged tokens.
         address creator;
         uint goal;
         uint pledged;
@@ -27,11 +27,13 @@ contract CrowdFund {
         bool claimed;
     }
 
-    IERC20 public immutable token;
-    uint public count;
-    uint public maxDuration;
+    IERC20 public immutable token; //to refer to the ERC-20 Token contract used for token transfers.
+    uint public count;             //to keep track of campaigns.
+    uint public maxDuration;       //Specify the maximum duration a campaign can be hosted.
     mapping(uint => Campaign) public campaigns;
-    mapping(uint => mapping(address => uint)) public pledgedAmount;
+    mapping(uint => mapping(address => uint)) public pledgedAmount; //a mapping to link the user's address and the number of tokens they pledged, and another mapping to link the campaign id
+
+//some events that will occur if a campaign is started, a token is pledged or unpledged, a campaign is cancelled, or a token is claimed or withdrawn.
 
     event Launch(
         uint id,
@@ -46,12 +48,24 @@ contract CrowdFund {
     event Claim(uint id);
     event Refund(uint id, address indexed caller, uint amount);
 
-    constructor(address _token, uint _maxDuration) {
+    constructor(address _token, uint _maxDuration) {    //ERC-20 token address and the maximum time a campaign can be hosted
         token = IERC20(_token);
         maxDuration = _maxDuration;
     }
 
-    function launch(uint _goal, uint32 _startAt, uint32 _endAt) external {
+/*
+function, "launch," which accepts the campaign's goal, the start timestamp, and the end timestamp, is now defined.
+Before starting the campaign, we first perform some checks.
+
+We determine whether the commencement time exceeds the current time.
+We make sure the end time is later than the beginning time.
+Finally, we make sure the campaign does not go beyond its maximum time.
+After that, we raise our count variable.
+The campaign information is then stored in the campaigns mapping, 
+with the count variable serving as the key and a struct as the value.
+*/
+
+    function launch(uint _goal, uint32 _startAt, uint32 _endAt) external { 
         require(_startAt >= block.timestamp,"Start time is less than current Block Timestamp");
         require(_endAt > _startAt,"End time is less than Start time");
         require(_endAt <= block.timestamp + maxDuration, "End time exceeds the maximum Duration");
@@ -69,6 +83,12 @@ contract CrowdFund {
         emit Launch(count,msg.sender,_goal,_startAt,_endAt);
     }
 
+/*
+function called cancel, 
+which allows the campaign's creator to end the campaign provided that they are the campaign's creator
+ and that the campaign has not yet begun.
+*/
+
     function cancel(uint _id) external {
         Campaign memory campaign = campaigns[_id];
         require(campaign.creator == msg.sender, "You did not create this Campaign");
@@ -77,6 +97,10 @@ contract CrowdFund {
         delete campaigns[_id];
         emit Cancel(_id);
     }
+
+/*
+ pledge functon feature, which asks for the campaign id and the number of tokens that need to be pledged
+*/ 
 
     function pledge(uint _id, uint _amount) external {
         Campaign storage campaign = campaigns[_id];
@@ -88,6 +112,10 @@ contract CrowdFund {
 
         emit Pledge(_id, msg.sender, _amount);
     }
+
+/*
+function called unpledge that removes the tokens that a user has pledged, just as the pledge function.
+*/
 
     function unPledge(uint _id,uint _amount) external {
         Campaign storage campaign = campaigns[_id];
@@ -102,6 +130,15 @@ contract CrowdFund {
         emit Unpledge(_id, msg.sender, _amount);
     }
 
+/*
+The creator can claim all of the tokens raised for the campaign with the help of a claim function that we define next if the following criteria are met.
+
+the campaign's originator is the one who called the function.
+The campaign has come to a close.
+The objective has been surpassed by the quantity of tokens raised (campaign succeded)
+The tokens have not yet been redeemed.
+*/
+
     function claim(uint _id) external {
         Campaign storage campaign = campaigns[_id];
         require(campaign.creator == msg.sender, "You did not create this Campaign");
@@ -115,6 +152,10 @@ contract CrowdFund {
         emit Claim(_id);
     }
 
+/*
+If the campaign is unsuccessful, 
+we create a refund function that allows users to withdraw their tokens from the contract.
+*/
     function refund(uint _id) external {
         Campaign memory campaign = campaigns[_id];
         require(block.timestamp > campaign.endAt, "not ended");
